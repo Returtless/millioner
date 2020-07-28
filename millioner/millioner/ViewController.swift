e23b86385646c8213b1d28b5bc034db1d919a672
Vladislav Likhachev <mymindissharp@gmail.com>
2020-07-28 20:45:30 +0300

    Пустой проект

---
+++ millioner/millioner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  millioner
+//
+//  Created by Владислав Лихачев on 28.07.2020.
+//  Copyright © 2020 Vladislav Likhachev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
