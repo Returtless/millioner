715efd39321bdcbdb0274ada9ad33125d054cc88
Vladislav Likhachev <mymindissharp@gmail.com>
2020-07-29 23:56:01 +0300

    Улучшил функционал и дизайн

--- millioner/millioner/Question.swift
+++ millioner/millioner/Question.swift
@@ -20,30 +20,3 @@ struct Question: Codable {
     var id: Int
 }
 
-class DataService {
-    static func getServerData(qType:Int, count:Int, completion: @escaping (_ array : [Question]?) -> Void) {
-        let params: Parameters = [
-            "qType": qType,
-            "count": count
-        ]
-        AF.request("https://engine.lifeis.porn/api/millionaire.php",
-                   parameters: params).responseJSON{ response in
-                    print(response)
-                    guard let data = response.data else { return }
-                    do {
-                        let res = try JSONDecoder().decode(Questions.self, from: data)
-                        var array: [Question]? = res.data
-                        if let questions = array {
-                            for i in 0..<questions.count {
-                                let rightAnswer = questions[i].answers[0]
-                                array![i].answers.shuffle()
-                                array![i].id = array![i].answers.firstIndex(of: rightAnswer)!
-                            }
-                        }
-                        completion(array)
-                    } catch {
-                        print("error")
-                    }
-        }
-    }
-}
